aad32597ed8a56aa4a82d2c478bd9f87d8309a25
UkaszSierak <46873675+UkaszSierak@users.noreply.github.com>
2019-02-14 00:13:33 +0100

    Update ImageProcessing.py
    - Adding description on top
    - correcting syntax

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -1,19 +1,28 @@
+"""
+    Image Processing module contains majority of functions which are used to preprocessed,
+    processed and analise image.
+
+    Functions:
+                viewImages() - only for testing purposes, views image
+                prepare() - held all preprocessing actions in image processing such as converting to grey, binarizing and applaying Canny filter,
+
+                shadowCorrection() - reduce shadow influence on image processing, performed on image ROIs which contains detected objects,
+                                     shadow reduce mechanism is described in function ShadowReduce() in test_c.pyx module
+                getObject() - extract all detected object ROIS from image
+                getContours - return contours data from image
+"""
+
 from functions import *
 import numpy
 import test_c
-import cv2 as cv
-import ObjectClass
-from database import *
-from trajectoryclass import *
-import RCPclass
 
-def View_images(images: list):
+def viewImages(images: list):
 
     for idx, item in enumerate(images):
         cv.imshow(str(idx), item )
     cv.waitKey()
 
-def Prepare(image: numpy.ndarray):
+def prepare(image: numpy.ndarray):
 
     image_container = image
 
@@ -25,12 +34,10 @@ def Prepare(image: numpy.ndarray):
 
     image_container = binarize(image_container)
     image_container = AutoCanny(image_container)
-    #View_images([image_container])
-
 
     return image_container
 
-def ShadowCorrection(contours ,image: np.ndarray):
+def shadowCorrection(contours ,image: np.ndarray):
 
     im = np.asanyarray(image, dtype=np.int)
     objects = []
@@ -55,79 +62,60 @@ def ShadowCorrection(contours ,image: np.ndarray):
 
     return objects, ref_points
 
-def GetObjects(image):
+def getObjects(image):
 
-    prepared_image = Prepare(image)
+    prepared_image = prepare(image)
     img, contours, hierarchy = cv.findContours(prepared_image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    objects, ref_points = ShadowCorrection(contours,image)
-
+    objects, ref_points = shadowCorrection(contours,image)
 
     return objects, ref_points
 
-def GetContours(objects: list):
+def getContours(objects: list):
 
     list = []
 
     for idx, object in enumerate(objects):
-        obrazek, kontury, hierarchia = cv.findContours(Prepare(object), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        obrazek, kontury, hierarchia = cv.findContours(prepare(object), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         list.append(kontury)
 
     return list
 
 if __name__ == '__main__':
+
+    import cv2 as cv
+    import Object
+    from database import *
+    from Trajectory import *
+    import RobotControlProgram
+
     image = cv.imread('test5.jpg')
     image = scaleUp(image)
     message = 'QoKX;290.62;Y;-0.09;Z;11.26;A;-179.94;B;-0.26;C;179.93;L1;0.00;;6,0;100;0.00;00000000'
     current_location = ExtractCoordinates(message)
-    objects, ref_points = GetObjects(image)
-    objects = [ ObjectClass.Object(image,object, ref_point)  for object, ref_point in zip(objects,ref_points)]
+    objects, ref_points = getObjects(image)
+    objects = [ Object.Object(image,object, ref_point)  for object, ref_point in zip(objects,ref_points)]
     containers = {'2block': (298.6117,0.00,339.8730,0.00,178.7203,0.00),
                   '4block': (327.6703,-252.0321,125.6951,0.00,178.7204,0.0000),
                   '3block': (237.3889,-252.0321,125.6951,0.00,178.7204,0.0000)}
 
     trajectory = Trajectory()
     for key, value in containers.items():
-        trajectory.AddNewPoint(value)
+        trajectory.addPoint(value)
 
     data = ReadFromDB()
     for item in objects:
-        item.AssignContainer(data)
+        item.assignContainer(data)
         container = item.container
-        x_delta, y_delta = item.GetFromCameraDistance()
+        x_delta, y_delta = item.getCamDistance()
         correction = {'X': current_location['X'] + x_delta,
                       'Y': current_location['Y'] + y_delta,
                       'A': item.rotation}
         new_location = GetNewLocation(correction, current_location)
         prepared_location = PrepareLocation(new_location)
 
-        trajectory.AddNewPoint(prepared_location)
-        trajectory.AddRelation(prepared_location,containers[container])
+        trajectory.addPoint(prepared_location)
+        trajectory.addRelation(prepared_location,containers[container])
 
-    robot = RCPclass.RobotControlProgram(trajectory.point_list, trajectory.point_relations, '/dev/USB')
-    robot.Generate()
-    robot.Chopp()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    robot = RobotControlProgram.RobotControlProgram(trajectory.point_list, trajectory.point_relations, '/dev/USB')
+    robot.generate()
+    robot.chopp()
